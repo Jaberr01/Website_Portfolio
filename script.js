96abd5e66d1b665588d3223725a8b8f9ac4fc925
Jaberr01 <mfjaber01@gmail.com>
2026-05-19 09:04:14 -0700

    many changes, including finishing more projects starting work on skils

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ boxes.forEach(box => {
             const boxCenterX = rect.left + rect.width / 2;
             const boxCenterY = rect.top + rect.height / 2;
 
-            const screenCenterX = window.innerWidth / 2;
+            const screenCenterX = (window.innerWidth) / 2;
             const screenCenterY = 500;
 
             const dx = screenCenterX - boxCenterX;
-            const dy = screenCenterY - boxCenterY;
+            const dy = screenCenterY - boxCenterY/2;
 
             box.style.transform = `translate(${dx}px, ${dy}px) scale(3)`;
             box.style.zIndex = 20;
